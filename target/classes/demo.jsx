28b37cec04e0b404c431bf7ca3f42dbb341c462f
AdvancedAntiSkid <47271339+AdvancedAntiSkid@users.noreply.github.com>
2023-07-23 14:36:48 +0200

    feat: remove target dir

--- target/classes/demo.jsx
+++
@@ -1,8 +0,0 @@
-const Demo = () => (
-    <div class="demo">
-        <div class="wrapper">
-            <h1>Void Framework</h1>
-            <p>This is a demo page</p>
-        </div>
-    </div>
-);
--- target/test/components/demo.jsx
+++
@@ -1,6 +0,0 @@
-<div class="container">
-    <div class="content">
-        <h1>Void Framework</h1>
-        <p>This is a demo page</p>
-    </div>
-</div>
